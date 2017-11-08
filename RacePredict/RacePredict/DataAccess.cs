cf21da4a1dc256b8476c403d375ca0fc1adb3e5f
chungontsui <chungont@gmail.com>
2017-11-08 23:12:50 +1300

    Created a new solution, setup EF (db first), empty Test project

---
+++ RacePredict/RacePredict/DataAccess.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RacePredict
+{
+	public class DataAccess
+	{
+		public IEnumerable<RaceDataEntities> GetRaceData()
+		{
+			using (var context = new dbNZGoodies_TestEntities())
+			{
+				return context.RaceDataEntities;
+			}
+		}
+	}
+}
---
+++ RacePredict/RacePredict/Repository.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RacePredict
+{
+	public class Repository
+	{
+		private DataAccess _da;
+
+		public Repository()
+		{
+			_da = new DataAccess();
+		}
+		public IEnumerable<RaceDataEntities> GetRaceDataByJockeyName(string Jockey)
+		{
+			if (string.IsNullOrEmpty(Jockey))
+			{
+				throw new Exception("Jockey Name is empty");
+			}
+
+			return _da.GetRaceData().Where(r => r.JockeyName.Contains(Jockey));
+		}
+
+		public IEnumerable<RaceDataEntities> GetRaceDataByHorseName(string Horse)
+		{
+			if (string.IsNullOrEmpty(Horse))
+			{
+				throw new Exception("Horse Name is empty");
+			}
+
+			return _da.GetRaceData().Where(r => r.HorseName.Contains(Horse));
+		}
+
+		public IEnumerable<RaceDataEntities> GetRaceDataByHorseId(int HorseId)
+		{
+			return _da.GetRaceData().Where(r => r.HorseID == HorseId);
+		}
+
+		public IEnumerable<RaceDataEntities> GetRaceDataByTrainerName(string Trainer)
+		{
+			if (string.IsNullOrEmpty(Trainer))
+			{
+				throw new Exception("Trainer Name is empty");
+			}
+
+			return _da.GetRaceData().Where(r => r.Trainer.Contains(Trainer));
+		}
+	}
+}
